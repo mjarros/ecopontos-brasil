d6751b46a063c46ed261d667a02f5015398bd1b1
Marcio Jarros <marciojarros@hotmail.com>
2025-09-06 10:03:10 -0300

    ajustes estilo na pagina de busca

--- src/app/_components/EcopontosListContainer/EcopontosListContainer.tsx
+++ src/app/_components/EcopontosListContainer/EcopontosListContainer.tsx
@@ -1,22 +1,26 @@
 "use client";
 
+import { Ecoponto } from "@/utils/types";
 import { FC, useState } from "react";
 import { Form } from "react-bootstrap";
 
-export default function EcopontosListContainer({ ecopontos, city }) {
+type EcopontosListContainerProps = {
+  ecopontos: Array<Ecoponto>;
+  city: string;
+};
+
+export default function EcopontosListContainer({ ecopontos, city }: EcopontosListContainerProps) {
   const [searchValue, setSearchValue] = useState<string>();
   return (
     <div className="list-container">
       <Form className="list-container__neighborhood">
-        <p>Se quiser, digite seu bairro...</p>
+        <p>Se quiser, digite parte do endereço...</p>
         <div className="custom-input-wrapper">
           <Form.Control className="custom-input" type="text" placeholder="Digite aqui..." value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
         </div>
       </Form>
-
-      <h2>
-        Ecopontos e reciclagens em até 20km de {city}: {ecopontos?.length} 👇🏻
-      </h2>
+      <h2>Ecopontos e reciclagens em até 20km de {city}</h2>
+      <h3>Quantidade: {ecopontos?.length} 👇🏻</h3>
       <p>* Quando disponível, o telefone é exibido. Dados com origem da API Google Maps.</p>
       {ecopontos && ecopontos?.length > 0 ? (
         <ul className="">
@@ -36,8 +40,8 @@ export default function EcopontosListContainer({ ecopontos, city }) {
 type EcopontoCardProps = {
   name: string;
   address: string;
-  openAt: string;
-  telephone: string;
+  openAt: string | null;
+  telephone: string | null;
 };
 
 const Card: FC<EcopontoCardProps> = ({ name, address, openAt, telephone }) => {
---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export type Ecoponto = {
+  id: string;
+  google_place_id: string;
+  nome: string;
+  endereco: string;
+  telefone: string | null;
+  horario: string | null;
+  cidade: string | null;
+  latitude: string | null;
+  longitude: string | null;
+  atualizado_em: string;
+};
